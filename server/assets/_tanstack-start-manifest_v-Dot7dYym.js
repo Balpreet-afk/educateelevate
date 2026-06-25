0c82b1aff64a523e97b302cfec3b02731587cc18
Balpreet Singh <balpreet.singh761@gmail.com>
2026-06-25 16:56:01 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-Dot7dYym.js
@@ -0,0 +1,20 @@
+//#region \0tanstack-start-manifest:v
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/home/balpreet/Downloads/educate/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/educateelevate/assets/index-BVFryOF2.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/educateelevate/assets/index-BVFryOF2.js"
+		} }]
+	},
+	"/": {
+		filePath: "/home/balpreet/Downloads/educate/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/educateelevate/assets/routes-C5QdrIY_.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
